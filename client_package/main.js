099cf58ea5689d4411136aa703a5fba17e13634e
Lukas Berwanger <lukas.berwanger@googlemail.com>
2016-12-21 00:59:24 +0100

    WIP - this is not working in production
    added events so other pkg's can hook into the minimap
    new tiles
    added player marker
    added rotation of map
    added css rules to position the map on the UI

--- client_package/main.js
+++ client_package/main.js
@@ -7,10 +7,57 @@
 
 'use strict';
 
-const ui = new WebUIWindow('minimap', 'package://jc3mp-minimap/ui/index.html', new Vector2(viewportSize.x, viewportSize.y));
+const ui = new WebUIWindow('minimap', 'package://jc3mp-minimap/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 ui.autoResize = true;
 
-jcmp.ui.AddEventHandler('getLocalPlayerPos', msg => {
-  const posi = [(-(Math.floor(localPlayer.GetRenderTransform(0.0).position.z)/80)), Math.floor(localPlayer.GetRenderTransform(0.0).position.x)/120];
-  jcmp.ui.BroadcastEvent('setLocalPlayerPos', JSON.stringify(posi));
+jcmp.ui.AddEventHandler('minimap_getLocalPlayerPos', msg => {
+  // TODO need to adjust this shit
+
+  //const posi = [(-(Math.floor(localPlayer.GetRenderTransform(0.0).position.z)/90)), Math.floor(localPlayer.GetRenderTransform(0.0).position.x)/116];
+  //const posi = [(-(Math.floor(localPlayer.GetRenderTransform(0.0).position.z + 10 ) / 1000)), Math.floor(localPlayer.GetRenderTransform(0.0).position.x -  65) / 1000];
+
+  const map_w = 32768 * 2;
+  const tiles_x = 16;
+
+  const x = localPlayer.GetRenderTransform(0.0).position.x;
+  const y = localPlayer.GetRenderTransform(0.0).position.z;
+
+  const posi = [(y * map_w / tiles_x - map_w / 2), (x * map_w / tiles_x - map_w / 2)];
+
+  posi[2] = y; // NOTE DEVstuff
+  posi[3] = x; // NOTE DEVstuff
+
+  jcmp.ui.BroadcastEvent('minimap_setLocalPlayerPos', JSON.stringify(posi));
+
+});
+
+jcmp.ui.AddEventHandler('minimap_getLocalPlayerRot', msg => {
+  jcmp.ui.BroadcastEvent('minimap_setLocalPlayerRot', localPlayer.rotation.y * 100);
+});
+
+jcmp.ui.AddEventHandler('minimap_ready', msg => {
+  jcmp.events.CallRemote('minimap_ready', msg);
+});
+
+jcmp.events.AddRemoteCallable('minimap_draw', drawcalls => {
+  drawcalls = JSON.parse(drawcalls);
+  jcmp.ui.BroadcastEvent('minimap_clear', 'Dennis best dude EUWEST');
+
+  for (let drawcall in drawcalls) {
+    if(drawcall['type'] == 'addCustomCSS') {
+      jcmp.ui.BroadcastEvent('minimap_addCustomCSS', JSON.stringify(drawcall));
+    }
+
+    if(drawcall['type'] == 'drawText') {
+      jcmp.ui.BroadcastEvent('minimap_addText', JSON.stringify(drawcall['data']));
+    }
+
+    if(drawcall['type'] == 'drawCircle') {
+      jcmp.ui.BroadcastEvent('minimap_addCircle', JSON.stringify(drawcall['data']));
+    }
+  }
+});
+
+jcmp.events.AddRemoteCallable('minimap_removeCSS', identifier => {
+  jcmp.ui.BroadcastEvent('minimap_removeCSS', identifier);
 });
--- main.js
+++ main.js
@@ -7,4 +7,72 @@
 
 'use strict';
 
+let drawcalls = [];
+
+jcmp.events.AddRemoteCallable('minimap_ready', (msg) => {
+  console.log(msg);
+  jcmp.events.CallRemote('minimap_draw', JSON.stringify(drawcalls));
+});
+
+/**
+ * Registers a new Drawcall
+ * 
+ * @param {string} pkg name of pkg which want's to add sth. to the minimap
+ * @param {string} identifier to identify the drawcall if you want to edit or remove them later
+ * @param {string} type type of Object
+ * @param {Object} data 
+ * 
+ * Expl jcmp.events.CallRemote('minimap_addDrawcall', 'billboards', 'billboards_board1', 'drawText', { x: 12, y: 13, text: 'Hello', fontSize: 5});
+ * Expl jcmp.events.CallRemote('minimap_addDrawcall', 'hungergames', 'hungergames_spawnradius', 'drawCircle', { x: 12, y: 15, radius: 10, color: '#000'});
+ * Expl jcmp.events.CallRemote('minimap_addDrawcall', 'billboards', 'billboards_css', 'addCustomCSS', { css: 'body { color: #fff; }'});
+ * 
+ */
+jcmp.events.AddRemoteCallable('minimap_addDrawcall', (pkg, identifier, type, data) => {
+  if(pkg == '' || identifier == '') {
+    console.log(`[minimap]\x1b[31mA Package requested to add a Drawcall without giving correct Informations!\x1b[0m`);
+  } else {
+    if(!(type == 'drawText' || type == 'drawCircle' || type == 'addCustomCSS')){
+      console.log(`[minimap]\x1b[31mThe Package ${pkg} requested to add a unknown Type of Drawcall (${identifier})(${type})\x1b[0m`);
+    } else {
+      drawcalls[identifier] = {
+        pkg: pkg,
+        identifier: identifier,
+        type: type,
+        data
+      };
+
+      jcmp.events.CallRemote('minimap_draw', JSON.stringify(drawcalls));
+    }
+  }
+});
+
+/**
+ * Removes a Drawcall
+ * 
+ * @param {string} pkg name of pkg which want's to remove sth. from the minimap
+ * @param {string} identifier to identify the drawcall if you want to edit or remove them later
+ * 
+ * Expl jcmp.events.CallRemote('minimap_removeDrawcall', 'billboards', 'billboards_board1');
+ * Expl jcmp.events.CallRemote('minimap_removeDrawcall', 'hungergames', 'hungergames_spawnradius');
+ * Expl jcmp.events.CallRemote('minimap_removeDrawcall', 'billboards', 'billboards_css');
+ * 
+ */
+jcmp.events.AddRemoteCallable('minimap_removeDrawcall', (pkg, identifier) => {
+  if(pkg == '' || identifier == '') {
+    console.log(`[minimap]\x1b[31mA Package requested to remove a Drawcall without giving correct Informations!\x1b[0m`);
+  } else {
+      const idofshit = drawcalls.indexOf(identifier);
+      
+      if (idofshit > -1) {
+        if(drawcalls[idofshit].type == 'addCustomCSS') {
+          jcmp.events.CallRemote('minimap_removeCSS', identifier);
+        }
+
+        drawcalls.splice(idofshit, 1);
+      }
+
+      jcmp.events.CallRemote('minimap_draw', JSON.stringify(drawcalls));
+    }
+});
+
 console.log('[minimap] initialized!');
