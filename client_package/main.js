56679d81e2504f3c38803a614b24489f28dd0942
Lukas Berwanger <lukas.berwanger@googlemail.com>
2017-01-06 01:03:42 +0100

    api changes

--- client_package/main.js
+++ client_package/main.js
@@ -10,7 +10,7 @@
 const ui = new WebUIWindow('minimap', 'package://jc3mp-minimap/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 ui.autoResize = true;
 
-jcmp.ui.AddEventHandler('minimap_getLocalPlayerPos', msg => {
+jcmp.ui.AddEvent('minimap_getLocalPlayerPos', msg => {
   // TODO need to adjust this shit
 
   //const posi = [(-(Math.floor(localPlayer.GetRenderTransform(0.0).position.z)/90)), Math.floor(localPlayer.GetRenderTransform(0.0).position.x)/116];
@@ -19,45 +19,45 @@ jcmp.ui.AddEventHandler('minimap_getLocalPlayerPos', msg => {
   const map_w = 32768 * 2;
   const tiles_x = 16;
 
-  const x = localPlayer.GetRenderTransform(0.0).position.x;
-  const y = localPlayer.GetRenderTransform(0.0).position.z;
+  const x = jcmp.localPlayer.GetRenderTransform(0.0).position.x;
+  const y = jcmp.localPlayer.GetRenderTransform(0.0).position.z;
 
   const posi = [(y * map_w / tiles_x - map_w / 2), (x * map_w / tiles_x - map_w / 2)];
 
   posi[2] = y; // NOTE DEVstuff
   posi[3] = x; // NOTE DEVstuff
 
-  jcmp.ui.BroadcastEvent('minimap_setLocalPlayerPos', JSON.stringify(posi));
+  jcmp.ui.CallEvent('minimap_setLocalPlayerPos', JSON.stringify(posi));
 
 });
 
-jcmp.ui.AddEventHandler('minimap_getLocalPlayerRot', msg => {
-  jcmp.ui.BroadcastEvent('minimap_setLocalPlayerRot', localPlayer.rotation.y * 100);
+jcmp.ui.AddEvent('minimap_getLocalPlayerRot', msg => {
+  jcmp.ui.CallEvent('minimap_setLocalPlayerRot', jcmp.localPlayer.rotation.y * 100);
 });
 
-jcmp.ui.AddEventHandler('minimap_ready', msg => {
+jcmp.ui.AddEvent('minimap_ready', msg => {
   jcmp.events.CallRemote('minimap_ready', msg);
 });
 
 jcmp.events.AddRemoteCallable('minimap_draw', drawcalls => {
   drawcalls = JSON.parse(drawcalls);
-  jcmp.ui.BroadcastEvent('minimap_clear', 'Dennis best dude EUWEST');
+  jcmp.ui.CallEvent('minimap_clear', 'Dennis best dude EUWEST');
 
   for (let drawcall in drawcalls) {
     if(drawcall['type'] == 'addCustomCSS') {
-      jcmp.ui.BroadcastEvent('minimap_addCustomCSS', JSON.stringify(drawcall));
+      jcmp.ui.CallEvent('minimap_addCustomCSS', JSON.stringify(drawcall));
     }
 
     if(drawcall['type'] == 'drawText') {
-      jcmp.ui.BroadcastEvent('minimap_addText', JSON.stringify(drawcall['data']));
+      jcmp.ui.CallEvent('minimap_addText', JSON.stringify(drawcall['data']));
     }
 
     if(drawcall['type'] == 'drawCircle') {
-      jcmp.ui.BroadcastEvent('minimap_addCircle', JSON.stringify(drawcall['data']));
+      jcmp.ui.CallEvent('minimap_addCircle', JSON.stringify(drawcall['data']));
     }
   }
 });
 
 jcmp.events.AddRemoteCallable('minimap_removeCSS', identifier => {
-  jcmp.ui.BroadcastEvent('minimap_removeCSS', identifier);
+  jcmp.ui.CallEvent('minimap_removeCSS', identifier);
 });
